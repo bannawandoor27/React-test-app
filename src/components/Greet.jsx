9fd2a182e7db9dc98491b196bb961aa8cf2f633a
hasanul banna <bannawandoor@gmail.com>
2023-02-15 13:52:23 +0530

    simple programs completed

---
+++ src/components/Greet.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// function Greet(){
+//     return (<h1>Hello Banna!</h1>
+//     )
+// }
+
+const Greet = () => <h1>Hello Banna!</h1>
+
+
+
+export default Greet;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component} from "react";
+
+class Welcome extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Welcome Banna!!!!</h1>
+                <p >ahhahah</p>
+            </div>
+        );
+        }
+}
+
+export default Welcome;
